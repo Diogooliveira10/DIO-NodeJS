c38f11a1daa68478677ccc81300b9122914030b8
Diogo Oliveira <ddiogooliveira9@gmail.com>
2021-11-02 19:34:38 -0300

    Melhorando o retorno da criação de servidor

--- introduction-nodejs/server.js
+++ introduction-nodejs/server.js
@@ -1,5 +1,7 @@
 const http = require('http')
+const host = 'http://localhost'
+const port = 3000
 
 http.createServer((req, res) => {
     res.end('<h1>Working</h1>')
-}).listen(3000, () => console.log('Server is running'))
+}).listen(port, () => console.log(`Server is running in ${host}:${port}`))
